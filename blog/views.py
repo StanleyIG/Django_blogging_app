a903e1bdd8d4f8485f3d14663dd77d0e6a03318f
StanleyIG <oootehts@gmail.com>
2023-06-10 13:43:13 +0300

    add mark dawn filter, tags filter

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 from taggit.models import Tag
 import os
 from dotenv import load_dotenv
@@ -53,11 +54,19 @@ def post_detail(request, year, month, day, post):
     # Форма для комментирования пользователями
     form = CommentForm()
 
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
